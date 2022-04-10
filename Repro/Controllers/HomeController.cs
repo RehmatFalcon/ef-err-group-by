2cfaea711144bd579ddc4213969588e922d81e4d
Kushal Niroula <niroulakushal31@gmail.com>
2022-04-10 16:46:38 +0545

    Initial

---
+++ Repro/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Repro.Data;
+using Repro.Models;
+
+namespace Repro.Controllers;
+
+public class Result
+{
+    public long Id { get; set; }
+
+    public Result(long id)
+    {
+        Id = id;
+    }
+
+    public Result()
+    {
+        
+    }
+}
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var x = _context.Products
+            .GroupBy(x => x.Id)
+            .Select(x => new Result(x.Key))
+            .Count();
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+    }
+}
---
+++ Repro/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Repro.Models;
+
+namespace Repro.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
